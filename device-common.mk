f4364c804f8d5aaf790fa16d59bcc8885cb85202
Sabar <sabar.pakpahan@gmail.com>
2020-11-16 16:07:41 +0700

    kang drm from angler

--- device-common.mk
+++ device-common.mk
@@ -135,8 +135,7 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service \
-    android.hardware.drm@1.2-service.clearkey
+    android.hardware.drm@1.0-service
 
 # Display
 PRODUCT_PACKAGES += \
